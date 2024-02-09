4eb4fb7235f5d66fe754f54298f75a905ebbdcf6
alex <al3xback@gmail.com>
2024-02-09 12:02:03 +0700

    added loading state

--- js/main.js
+++ js/main.js
@@ -2,38 +2,44 @@ import { sendHttpRequest } from './util.js';
 
 const cardsEl = document.querySelector('.cards');
 const cardTemplate = document.getElementById('card-template');
+const loadingEl = document.querySelector('.loading');
 
 const URL =
 	'https://gist.githubusercontent.com/al3xback/c9f5e86a2664af7f17a38792e5e5672e/raw/5fbd1d23464766fa061c8a0fc8c76c758f9e2427/3-column-data.txt';
 
-const renderCardContent = (data) => {
+const renderCardsContent = (data) => {
 	const resData = data.split('\n');
 	const size = resData.indexOf('');
 	const filteredData = resData.filter((item) => Boolean(item));
 
-	const cardData = [];
+	const carsData = [];
 	for (let i = 0; i < filteredData.length; i += size) {
-		cardData.push(filteredData.slice(i, i + size));
+		carsData.push(filteredData.slice(i, i + size));
 	}
 
-	for (const card of cardData) {
-		const [title, desc] = card;
-		const clone = document.importNode(cardTemplate.content, true);
+	loadingEl.parentElement.removeChild(loadingEl);
 
-		const cardEl = clone.querySelector('.card');
-		cardEl.classList.add('card--' + title.toLowerCase());
+	for (const car of carsData) {
+		const [name, description] = car;
+
+		const cardTemplateNode = document.importNode(
+			cardTemplate.content,
+			true
+		);
+		const cardEl = cardTemplateNode.querySelector('.card');
+		cardEl.classList.add('card--' + name.toLowerCase());
 
 		const cardImageEl = cardEl.querySelector('.card__img');
-		cardImageEl.src = './images/icons/' + title.toLowerCase() + '.svg';
+		cardImageEl.src = './images/icons/' + name.toLowerCase() + '.svg';
 
 		const cardTitleEl = cardEl.querySelector('.card__title');
-		cardTitleEl.textContent = title;
+		cardTitleEl.textContent = name;
 
 		const cardDescEl = cardEl.querySelector('.card__desc');
-		cardDescEl.textContent = desc;
+		cardDescEl.textContent = description;
 
-		cardsEl.appendChild(cardEl);
+		cardsEl.appendChild(cardTemplateNode);
 	}
 };
 
-sendHttpRequest('GET', URL, renderCardContent);
+sendHttpRequest('GET', URL, renderCardsContent);
